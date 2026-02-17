458976ec7f5dee1aa5767328649f12a6d2e5bf3e
Saanthanu Prasad <mail.saanthanuprasad@gmail.com>
2026-02-17 16:24:23 +0530

    learning notes upload

---
+++ learning_notes/DOM Challenge Starting Files/index.js
@@ -0,0 +1,5 @@
+document.query
+document.querySelector("#list a").style.color = "red";
+document.querySelector("button").style.backgroundColor = "yellow";
+document.querySelector("button").style.borderColor = "red";
+document.querySelector("h1").classList.add("huge");
